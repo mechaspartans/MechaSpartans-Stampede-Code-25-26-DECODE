eafffc42eb60b02b90c44d37988cebfa0a9fd8ee
MechAgoge <mechagoge@gmail.com>
2025-11-05 19:21:37 -0800

    limelight controlling angle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele_code/Limelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele_code/Limelight.java
@@ -64,7 +64,7 @@ public class Limelight extends LinearOpMode {
                         } else if (result.getTa() < 1.1 && result.getTa() > 0.9) {
                             if (result.getBotpose().getPosition().x >= 0.1) {
                                 stampede.drive(0, 0.25, 0, telemetry);
-                            } else if (result.getBotpose().getPosition().y <= -0.1)
+                            } else if (result.getBotpose().getPosition().x <= -0.1)
                                 stampede.drive(0, -0.25, 0, telemetry);
                         }
                     }
